2953a3043dc1ef2b9527366ecae4d796e2b33d27
Adela Tatum <80791686+addy-luna@users.noreply.github.com>
2024-03-15 19:16:40 -0700

    adding an express.js server

---
+++ express.js
@@ -0,0 +1,11 @@
+const app = express();
+app.use(express.static(initial_path));
+app.use(fileupload());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "home.html"));
+})
+
+app.listen("3000", () => {
+    console.log('listening......');
+})
